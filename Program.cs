96cdfe203a9773aadf33f936cab2c2652bd84f55
Nimap <Nimap@DESKTOP-G0JUIIV>
2023-10-30 14:49:10 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_task
+{
+    public class Person
+    {
+        public int id;
+        public string name;
+     public Person(int id,string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        public void display()
+        {
+            Console.WriteLine(id+" "+name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person(1,"dhirendra");
+            p.display();
+            Console.ReadLine();
+        }
+    }
+}
